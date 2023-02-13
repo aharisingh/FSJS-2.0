ce2d28362da581329bbb9ce9294d8d27d05ceb99
aharisingh <harivanshkashyap04@gmail.com>
2023-02-13 10:17:06 +0530

    code for DOM Project 6

---
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/06_DOM Project/06_DOM Project/Assignment6/index.js
@@ -0,0 +1,6 @@
+//TASK 1
+let headerImg =  document.querySelector("header img");
+headerImg.src = "./assets/ineuron-logo.png";
+
+//TASK 2
+document.querySelector(".app_price span").innerText = "$10";
